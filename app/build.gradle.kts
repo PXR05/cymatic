655c50739192a646f1f523d34b8cde1057eb9160
PXR <pxr2709@gmail.com>
2026-05-25 22:49:24 +0700

    chore: update version code and name to 0.1.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.pxr.cymatic"
         minSdk = 31
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.16"
+        versionCode = 161
+        versionName = "0.1.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
