fdbede9e46d88a55eb0abadbf35dd81e29bc32c0
PXR <pxr2709@gmail.com>
2026-06-13 18:23:29 +0700

    chore: update version to 0.1.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.pxr.cymatic"
         minSdk = 31
         targetSdk = 36
-        versionCode = 190
-        versionName = "0.1.9.0"
+        versionCode = 191
+        versionName = "0.1.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
